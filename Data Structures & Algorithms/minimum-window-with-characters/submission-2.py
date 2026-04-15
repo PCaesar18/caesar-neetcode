5c5efcb3671ea50dfd20bd8d651e22874ef3c9ad
Caesar <35256698+PCaesar18@users.noreply.github.com>
2026-04-15 13:01:43 +0200

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,52 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        result = ""
+        if len(s) < len(t):
+            return result 
+
+        best_len = float('inf')
+        l = 0
+
+        tmap = Counter(t)
+        window = Counter()
+
+        have = 0 #Number of characters that currently meet required frequency
+        need = len(tmap) #Total number of distinct characters we need to satisfy
+
+        for r in range(len(s)):
+            window[s[r]] += 1
+
+            if s[r] in tmap and window[s[r]] == tmap[s[r]]:
+                have += 1
+            while have == need:
+                if r - l + 1 < best_len:
+                    best_len = r - l + 1
+                    result = s[l:r + 1]
+                window[s[l]] -= 1
+
+                if s[l] in tmap and window[s[l]] < tmap[s[l]]:
+                    have -= 1
+                l += 1
+        return result 
+
+            
+
+
+
+        
+
+        return result 
+            
+
+
+
+
+
+
+        #work on new orgs 
+        # apply for jobs
+        # more programming 
+        #training
+        # meet Pep this week
+        # chillz with alfi & dide 
+        
