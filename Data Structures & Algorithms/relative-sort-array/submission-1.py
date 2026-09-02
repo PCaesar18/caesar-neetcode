b0691e02396181ac6ed2e12a83d88061566070b4
Caesar <35256698+PCaesar18@users.noreply.github.com>
2026-09-02 12:30:00 +0100

    Add: relative-sort-array - submission-1

---
+++ Data Structures & Algorithms/relative-sort-array/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        count = {}
+
+        for num in arr1:
+            count[num] = count.get(num, 0) + 1
+
+        result = []
+
+        for num in arr2:
+            result.extend([num] * count[num])
+            del count[num]
+
+        for num in range(1001):
+            if num in count:
+                result.extend([num] * count[num])
+
+        return result 
+                
+        
